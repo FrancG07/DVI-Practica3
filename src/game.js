36855a2d3b2546b7b5dd406f4c8ebb36a9c89e1d
FrancG07 <francg07@ucm.es>
2021-04-19 10:52:18 +0200

    Ventanas fin del juego
    - Añadidas ventanas que se muestran cuando se acaba el juego, ya sea por ganar o por perder

--- src/game.js
+++ src/game.js
@@ -1,13 +1,13 @@
 var game = function() {
 
 var Q = window.Q = Quintus()
-		.include(["Sprites", "Scenes", "Input", "2D", "UI", "Anim", "TMX"])
+		.include(["Sprites", "Scenes", "Input", "2D", "UI", "Anim", "TMX", "Touch"])
         .setup("myGame", {
 			width: 800,
 			height: 600,
 			scaleToFit: true
 		})
-        .controls();
+        .controls().touch();
 
 
 	Q.Sprite.extend("Mario", {
@@ -26,6 +26,12 @@ var Q = window.Q = Quintus()
 			if(this.p.y > 600){
 				this.respawn();	
 			}
+		},
+		die : function(){
+			Q.stage().pause();
+			Q.stageScene("endGame",1, {
+				label: "You lose"
+			});
 		},
 		respawn : function(){
 			this.p.x = 100;
@@ -65,7 +71,7 @@ var Q = window.Q = Quintus()
 
 			this.on("bump.left,bump.right,bump.bottom",function(collision) {
 				if(collision.obj.isA("Mario")) { 
-					collision.obj.respawn();
+					collision.obj.die();
 				}
 			});
 		}
@@ -91,7 +97,7 @@ var Q = window.Q = Quintus()
 
 			this.on("bump.left,bump.right,bump.bottom",function(collision) {				
 				if(collision.obj.isA("Mario")) { 
-					collision.obj.respawn();
+					collision.obj.die();
 				}else{
 					this.p.vy = -200;
 				}
@@ -116,8 +122,10 @@ var Q = window.Q = Quintus()
 		},
 		collision: function(col){
 			if(col.obj.isA("Mario")){
-				// Mostrar victoria
-				Q.stageScene("level1");
+				Q.stage().pause();
+				Q.stageScene("endGame",1, {
+					label: "You win!"
+				});
 			}
 		}
 	});
@@ -150,6 +158,30 @@ var Q = window.Q = Quintus()
 		        mario.destroy();
 		    });
 	   });
+
+	   Q.scene('endGame',function(stage) {
+			var container = stage.insert(new Q.UI.Container({
+				x: Q.width/2, y: Q.height/2, fill: "rgba(0,0,0,0.5)"
+			}));
+			var button = container.insert(new Q.UI.Button({
+				x: 0, y: 0, fill: "#CCCCCC", label: "Play Again"
+			}));
+			var label = container.insert(new Q.UI.Text({
+				x:10, y: -10 - button.p.h, label: stage.options.label
+			}));
+			// When the button is clicked, clear all the stages
+			// and restart the game.
+			button.on("click",function() {
+				Q.clearStages();
+				Q.stageScene('level1');
+			});
+
+			// Expand the container to visibly fit it's contents
+			// (with a padding of 20 pixels)
+			container.fit(20);
+		});
+
+
 
 	   Q.stageScene("level1");
 
